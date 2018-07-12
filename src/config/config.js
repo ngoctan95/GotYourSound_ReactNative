0c1e3e5c040bd50c84beb16559d2278e9f334249
David <david@nustechnology.com>
2018-07-12 15:22:35 +0700

    Init all git and source

---
+++ src/config/config.js
@@ -0,0 +1,3 @@
+import React from 'react';
+import {TabNavigator, StackNavigator} from 'react-navigation';
+import {Icon, Button} from 'react-native-elements';
---
+++ src/screens/MainScreen/MainScreen.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import {
+    Text,
+    View
+} from 'react-native';
+
+export default class MainScreen extends Component{
+    render(){
+        return(
+            <View>
+                    
+            </View>
+        )
+    }
+}
