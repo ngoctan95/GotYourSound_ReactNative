790afe3294342b9932e1fc967da4f88873dc3a51
NashelTan <nguyenngoctan44@gmail.com>
2018-07-12 15:33:18 +0700

    Add screens to config routers

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,6 @@
 import React from 'react';
 import {TabNavigator, StackNavigator} from 'react-navigation';
 import {Icon, Button} from 'react-native-elements';
+
+import MainScreen from '../screens/MainScreen/MainScreen';
+import SplashScreen from '../screens/SplashScreen/SplashScreen';
